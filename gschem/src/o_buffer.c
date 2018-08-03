lepton-eda/lepton-eda

libgeda: remove unused parameter
Removed unused parameter, make function prototypes uniform, and moved the object
parameter to the beginning of the list to follow the "this pointer" convention.

gschem/src/o_buffer.c
@@ -190,8 +190,9 @@ o_buffer_paste_start(GschemToplevel *w_current, int w_x, int w_y, int buf_num)
   x = snap_grid (w_current, rleft);
   y = snap_grid (w_current, rtop);
 
-  geda_object_list_translate (toplevel, w_x - x, w_y - y,
-                              toplevel->page_current->place_list);
+  geda_object_list_translate (toplevel->page_current->place_list,
+                              w_x - x,
+                              w_y - y);
 
   i_set_state(w_current, PASTEMODE);
   o_place_start (w_current, w_x, w_y);

gschem/src/o_move.c
@@ -119,12 +119,12 @@ void o_move_end_lowlevel (GschemToplevel *w_current,
     case (OBJ_BUS):
     case (OBJ_PIN):
       s_conn_remove_object_connections (page->toplevel, object);
-      geda_object_translate (page->toplevel, diff_x, diff_y, object);
+      geda_object_translate (object, diff_x, diff_y);
       s_conn_update_object (page, object);
       break;
 
     default:
-      geda_object_translate (page->toplevel, diff_x, diff_y, object);
+      geda_object_translate (object, diff_x, diff_y);
       break;
   }
 }

gschem/src/o_place.c
@@ -104,7 +104,7 @@ void o_place_end (GschemToplevel *w_current,
     page->place_list = NULL;
   }
 
-  geda_object_list_translate (page->toplevel, w_diff_x, w_diff_y, temp_dest_list);
+  geda_object_list_translate (temp_dest_list, w_diff_x, w_diff_y);
 
   /* Attach each item back onto the page's object list. Update object
    * connectivity and add the new objects to the selection list.*/

libgeda/src/scheme_object.c
@@ -2082,7 +2082,7 @@ SCM_DEFINE (translate_object_x, "%translate-object!", 3, 0, 0,
   int dy = scm_to_int (dy_s);
 
   o_emit_pre_change_notify (toplevel, obj);
-  geda_object_translate (toplevel, dx, dy, obj);
+  geda_object_translate (obj, dx, dy);
   o_emit_change_notify (toplevel, obj);
   o_page_changed (toplevel, obj);
 

rlutz/geda-gaf

gschem/src/o_buffer.c
@@ -190,8 +190,8 @@ o_buffer_paste_start(GschemToplevel *w_current, int w_x, int w_y, int buf_num)
   x = snap_grid (w_current, rleft);
   y = snap_grid (w_current, rtop);
 
-  o_glist_translate_world (toplevel, w_x - x, w_y - y,
-                           toplevel->page_current->place_list);
+  o_glist_translate_world (toplevel->page_current->place_list,
+                           w_x - x, w_y - y);
 
   i_set_state(w_current, PASTEMODE);
   o_place_start (w_current, w_x, w_y);

gschem/src/o_move.c
@@ -119,12 +119,12 @@ void o_move_end_lowlevel (GschemToplevel *w_current,
     case (OBJ_BUS):
     case (OBJ_PIN):
       s_conn_remove_object_connections (page->toplevel, object);
-      o_translate_world (page->toplevel, diff_x, diff_y, object);
+      o_translate_world (object, diff_x, diff_y);
       s_conn_update_object (page, object);
       break;
 
     default:
-      o_translate_world (page->toplevel, diff_x, diff_y, object);
+      o_translate_world (object, diff_x, diff_y);
       break;
   }
 }

gschem/src/o_place.c
@@ -104,7 +104,7 @@ void o_place_end (GschemToplevel *w_current,
     page->place_list = NULL;
   }
 
-  o_glist_translate_world(page->toplevel, w_diff_x, w_diff_y, temp_dest_list);
+  o_glist_translate_world(temp_dest_list, w_diff_x, w_diff_y);
 
   /* Attach each item back onto the page's object list. Update object
    * connectivity and add the new objects to the selection list.*/

libgeda/src/scheme_object.c
@@ -2082,7 +2082,7 @@ SCM_DEFINE (translate_object_x, "%translate-object!", 3, 0, 0,
   int dy = scm_to_int (dy_s);
 
   o_emit_pre_change_notify (toplevel, obj);
-  o_translate_world (toplevel, dx, dy, obj);
+  o_translate_world (obj, dx, dy);
   o_emit_change_notify (toplevel, obj);
   o_page_changed (toplevel, obj);
 