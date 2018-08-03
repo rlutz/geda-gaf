lepton-eda/lepton-eda

libgeda: remove unused parameter

gschem/src/x_clipboard.c
@@ -55,7 +55,7 @@ clip_get (GtkClipboard *cb, GtkSelectionData *selection_data,
   if (info != CLIP_TYPE_SCHEMATIC) return;
   /* Convert the objects in the clipboard buffer to gEDA schematic
    * format */
-  buf = geda_object_list_to_buffer (w_current->clipboard_buffer, toplevel);
+  buf = geda_object_list_to_buffer (w_current->clipboard_buffer);
   /* Set the selection appropriately */
   gtk_selection_data_set (selection_data, type,
                           8, /* 8-bit data (UTF-8) */

libgeda/src/scheme_page.c
@@ -391,7 +391,7 @@ SCM_DEFINE (page_to_string, "%page->string", 1, 0, 0,
   PAGE *page = edascm_to_page (page_s);
   TOPLEVEL *toplevel = edascm_c_current_toplevel ();
 
-  gchar *buf = geda_object_list_to_buffer (s_page_objects (page), toplevel);
+  gchar *buf = geda_object_list_to_buffer (s_page_objects (page));
   scm_dynwind_begin (0);
   scm_dynwind_unwind_handler (g_free, buf, SCM_F_WIND_EXPLICITLY);
   SCM result = scm_from_utf8_string (buf);

rlutz/geda-gaf

gschem/src/x_clipboard.c
@@ -55,7 +55,7 @@ clip_get (GtkClipboard *cb, GtkSelectionData *selection_data,
   if (info != CLIP_TYPE_SCHEMATIC) return;
   /* Convert the objects in the clipboard buffer to gEDA schematic
    * format */
-  buf = o_save_buffer (toplevel, w_current->clipboard_buffer);
+  buf = o_save_buffer (w_current->clipboard_buffer);
   /* Set the selection appropriately */
   gtk_selection_data_set (selection_data, type,
                           8, /* 8-bit data (UTF-8) */

libgeda/src/scheme_page.c
@@ -391,7 +391,7 @@ SCM_DEFINE (page_to_string, "%page->string", 1, 0, 0,
   PAGE *page = edascm_to_page (page_s);
   TOPLEVEL *toplevel = edascm_c_current_toplevel ();
 
-  gchar *buf = o_save_buffer (toplevel, s_page_objects (page));
+  gchar *buf = o_save_buffer (s_page_objects (page));
   scm_dynwind_begin (0);
   scm_dynwind_unwind_handler (g_free, buf, SCM_F_WIND_EXPLICITLY);
   SCM result = scm_from_utf8_string (buf);