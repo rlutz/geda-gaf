ae45aa1099623635f905eadf6e99f9c1534ffd3a
Roland Lutz <rlutz@hedmen.org>
2019-01-11 12:25:11 +0100

    gsymcheck: Use parallel test harness

--- gsymcheck/tests/runtest.sh
+++ gsymcheck/tests/runtest.sh
@@ -1,47 +1,19 @@
 #!/bin/sh
 
-INPUT=$1
-BUILDDIR=$2
-SRCDIR=$3
-rundir=${BUILDDIR}/run
+srcdir="`dirname $1`"
+stem="`basename $1 .output`"
 
-# create temporary run directory and required subdirs
-mkdir -m 0700 -p ${rundir}
-rc=$?
-if test $rc -ne 0 ; then
-	echo "Failed to create directory ${rundir} with appropriate permissions"
-	echo "mkdir returned $rc"
-	exit 1
-fi
+in="${srcdir}/${stem}.sym"
+ref="${srcdir}/${stem}.output"
+new="${stem}.new"
 
-TESTDIR=${BUILDDIR}
-export TESTDIR
+../src/gsymcheck -vv "${in}" |
+	grep -v "gEDA/gsymcheck version" |
+	grep -v "ABSOLUTELY NO WARRANTY" |
+	grep -v "This is free software" |
+	grep -v "the COPYING file" |
+	grep -v "Checking: " |
+	grep -v '^$' > "${new}"
 
-symbasename=`basename $INPUT .sym`
-
-in="${INPUT}"
-ref="${SRCDIR}/${symbasename}.output"
-new="${rundir}/new_${symbasename}.output"
-tmpfile=${rundir}/tmp$$
-
-${BUILDDIR}/../src/gsymcheck -vv ${in} 1> ${tmpfile} 2> ${rundir}/allerrors.output
-
-cat ${tmpfile} | \
-	grep -v "gEDA/gsymcheck version" | \
-	grep -v "ABSOLUTELY NO WARRANTY" | \
-	grep -v "This is free software" | \
-	grep -v "the COPYING file" | \
-	grep -v "Checking: " | \
-	grep -v '^$' > ${new}
-rm -f ${tmpfile}
-
-diff "${ref}" "${new}"
-status=$?
-
-rm -rf $rundir
-
-if test $status -ne 0; then
-	exit 2
-fi
-
-exit 0
+diff "${ref}" "${new}" || exit 2
+rm "${new}"
