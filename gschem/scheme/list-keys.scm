b020a08910f7298809054cc8d5917ff54674f049
Roland Lutz <rlutz@hedmen.org>
2019-02-13 19:50:22 +0100

    gschem: Update "list-keys.scm" to current API

--- gschem/scheme/list-keys.scm
+++ gschem/scheme/list-keys.scm
@@ -1,6 +1,7 @@
 ;; Printing out current key bindings for gEDA (gschem)
 ; Stefan Petersen 1999-04-04 (spe@stacken.kth.se)
 ; Free for all use. Just don't blame me when your house burns up.
+; Updated by Roland Lutz 2019-02-13
 
 (define (print-mapped-keys mapped-keys)
   (display (car mapped-keys))
@@ -13,15 +14,12 @@
   (newline))
 
 (define (mapping-keys keymap keys)
-  (for-each (lambda (mapped-key) ; Receives a pair
-	      (let ((action (eval (cdr mapped-key))))
-		(cond ((list? action)
-		       (mapping-keys action (append keys (car mapped-key))))
-		      (else
-		       (print-mapped-keys (list  ; was print
-					   (cdr mapped-key)
-					   keys 
-					   (car mapped-key)))))))
-	    keymap))
+  (keymap-for-each
+   (lambda (key binding)
+     (cond ((keymap? binding)
+	    (mapping-keys binding (append keys key)))
+	   (else
+	    (print-mapped-keys (list binding keys key)))))
+   keymap))
 
-(mapping-keys global-keymap '())
+(mapping-keys %global-keymap '())
