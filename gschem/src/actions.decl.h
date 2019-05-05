a667f9ed535edcd9286eb2f3c9f37706483f2a88
Roland Lutz <rlutz@hedmen.org>
2019-05-05 13:18:04 +0200

    gschem: Document action struct auto-generation

--- gschem/src/actions.decl.h
+++ gschem/src/actions.decl.h
@@ -1,3 +1,43 @@
+/* gEDA - GPL Electronic Design Automation
+ * gschem - gEDA Schematic Capture
+ * Copyright (C) 1998-2010 Ales Hvezda
+ * Copyright (C) 1998-2019 gEDA Contributors (see ChangeLog for details)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*! \file actions.decl.h
+ *  \brief Generated action declarations.
+ *
+ * This file is preprocessed into actions.decl.x which contains
+ * variable declarations for the individual actions defined in
+ * actions.c.  The resulting file looks like this:
+ *
+ *     GschemAction *action_file_new;
+ *     GschemAction *action_file_new_window;
+ *     GschemAction *action_file_open;
+ *     ...
+ *
+ * Source files which reference individual actions by name (e.g. in
+ * order to change their sensitivity) should include this file as
+ *
+ *     #include "actions.decl.x"
+ *
+ * Don't include the un-processed file directly--this won't work.
+ */
+
 #define DEFINE_ACTION(c_id, id, icon, name, label, menu_label, tooltip, type) \
   KEEP_LINE GschemAction *action_ ## c_id;
 #include "actions.c"
--- gschem/src/actions.init.c
+++ gschem/src/actions.init.c
@@ -1,3 +1,45 @@
+/* gEDA - GPL Electronic Design Automation
+ * gschem - gEDA Schematic Capture
+ * Copyright (C) 1998-2010 Ales Hvezda
+ * Copyright (C) 1998-2019 gEDA Contributors (see ChangeLog for details)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/*! \file actions.init.c
+ *  \brief Generated action initializations.
+ *
+ * This file is preprocessed into actions.init.x which contains a call
+ * to \ref gschem_action_register for each individual action defined
+ * in actions.c.  The resulting file looks like this:
+ *
+ *     action_file_new = gschem_action_register (
+ *                         "file-new",
+ *                         "gtk-new",
+ *                         _("New File"),
+ *                         _("New"),
+ *                         _("_New"),
+ *                         NULL,
+ *                         GSCHEM_ACTION_TYPE_ACTUATE,
+ *                         action_callback_file_new);
+ *     ...
+ *
+ * actions.init.x should only be included once in the definition of
+ * \ref init_module_gschem_core_builtins.
+ */
+
 #define DEFINE_ACTION(c_id, id, icon, name, label, menu_label, tooltip, type) \
   KEEP_LINE                                                                   \
   action_ ## c_id =                                                           \
